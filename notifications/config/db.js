da02d81f8df7c8050755c274e70f218aee2bf20c
Elnara <eemerbekova@gmail.com>
2024-03-28 22:21:04 +0300

    JT-B-E-014 | connect MongoDB, create db models (notifications, notification_settings)

---
+++ notifications/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./index');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ notifications/config/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PORT: process.env.PORT || 3002,
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/notifications'
+  };
