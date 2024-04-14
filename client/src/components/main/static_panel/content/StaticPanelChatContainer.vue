b808ca8f705e805225e07bc730bccd6c893fd546
VovaDVA <vl82d@mail.ru>
2024-04-14 16:45:06 +0300

    [JT-F-021] implemented content search in static panel

--- client/src/components/main/static_panel/content/StaticPanelChatContainer.vue
+++ client/src/components/main/static_panel/content/StaticPanelChatContainer.vue
@@ -24,7 +24,7 @@ export default {
                     return 'StaticPanelMessenger';
                 default:
                     return 'StaticPanelChats';
-            }
+            } 
         }
     },
     methods: {
