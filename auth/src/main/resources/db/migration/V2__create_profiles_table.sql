8daa13999d3349e85f2dceabaea3beae73ce36c0
PrimakMV <pm2710@mail.ru>
2024-04-23 10:00:32 +0300

    [JT-B-J-041] Minor changes have been made: the binding key update script has been moved to the table creation script.

--- auth/src/main/resources/db/migration/V2__create_profiles_table.sql
+++ auth/src/main/resources/db/migration/V2__create_profiles_table.sql
@@ -15,4 +15,11 @@ CREATE TABLE "profiles"
     	ON UPDATE CASCADE
 );
 
+ALTER TABLE profiles DROP CONSTRAINT fk410q61iev7klncmpqfuo85ivh;
+
+ALTER TABLE profiles
+    ADD CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE;
 
