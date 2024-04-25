849533d3be23d978eacdcaa51b742a0636263f69
Lisa <ryabyhina.liza@mail.ru>
2024-04-25 22:27:37 +0300

    [JT-B-J-027] Added changes to the method for updating profile fields in the database, added interaction with a token and sending requests to obtain a profile model using a connector with an authorization method. The ProfilesDTO entity has been added, containing a set of fields that come in a request for updating a profile. Added a converter for converting Profiles and ProfileBom models into each other. Changed the logic in the controller and service to update the main profile fields.

--- profile/src/main/java/com/jointProfile/ProfileApplication.java
+++ profile/src/main/java/com/jointProfile/ProfileApplication.java
@@ -2,13 +2,14 @@ package com.jointProfile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ProfileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileApplication.class, args);
-
 	}
 
 }
---
+++ profile/src/main/java/com/jointProfile/connector/AuthConnector.java
@@ -0,0 +1,13 @@
+package com.jointProfile.connector;
+
+import com.jointProfile.bom.ProfileBom;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "auth-connector", url = "${connector.auth.url}")
+public interface AuthConnector {
+
+    @GetMapping(path = "/getCurrentProfile") // подключение к методу в авторизации
+    ProfileBom getCurrentProfile(@RequestHeader(name = "Authorization") String token);
+}
