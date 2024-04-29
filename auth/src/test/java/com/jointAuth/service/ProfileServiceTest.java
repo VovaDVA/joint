cfc2da7e92bb32ce9d98840ecb316497d7528943
PrimakMV <pm2710@mail.ru>
2024-04-29 20:03:14 +0300

    [JT-B-J-058] Added unit tests for the method of sending a confirmation email to log in from 2FA.

--- auth/src/test/java/com/jointAuth/service/ProfileServiceTest.java
+++ auth/src/test/java/com/jointAuth/service/ProfileServiceTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Optional;
 
