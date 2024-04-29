8be80ce1688d0ebefe73ba5e0e6f43e425c6b6a1
PrimakMV <pm2710@mail.ru>
2024-04-29 14:12:01 +0300

    [JT-B-J-057] Added testing of the automatic database cleaning method by timer for password reset confirmation codes.

--- auth/src/main/resources/db/migration/V3__create_two_factor_auth_verification_codes_table.sql
+++ auth/src/main/resources/db/migration/V3__create_two_factor_auth_verification_codes_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "two_factor_auth_verification_codes"
 (
     id BIGINT PRIMARY KEY,
-    user_id BIGINT NOT NULL ,
-    code VARCHAR(6),
+    user_id BIGINT NOT NULL,
+    code VARCHAR(6) NOT NULL,
     expiration_time TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP + INTERVAL '2 minutes'
 );
