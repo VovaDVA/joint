ce52cc09be6fe3a7128d7246cd6d79ea99e803f4
PrimakMV <pm2710@mail.ru>
2024-04-30 22:04:10 +0300

    [JT-B-J-067] Added integration tests for the user deletion request method.

--- auth/src/test/java/com/jointAuth/integration/controller/UserDeleteControllerIntegrationTest.java
+++ auth/src/test/java/com/jointAuth/integration/controller/UserDeleteControllerIntegrationTest.java
@@ -1,121 +1,94 @@
 package com.jointAuth.integration.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.jointAuth.model.profile.Profile;
-import com.jointAuth.model.user.User;
-import com.jointAuth.repository.ProfileRepository;
-import com.jointAuth.repository.UserRepository;
+import com.jointAuth.service.UserService;
 import com.jointAuth.util.JwtTokenUtils;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.ResultActions;
 
-import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserDeleteControllerIntegrationTest {
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private ProfileRepository profileRepository;
-
-    @Autowired
-    private JwtTokenUtils jwtTokenUtils;
-
     private MockMvc mockMvc;
 
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private JwtTokenUtils jwtTokenUtils;
+
     @BeforeEach
-    public void setup() {
+    public void setUp() {
 
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        when(jwtTokenUtils
+                .getCurrentUserId(anyString()))
+                .thenReturn(1L);
     }
 
     @Test
-    @Transactional
-    public void testDeleteUserSuccess() throws Exception {
-        User testUser = new User();
-        testUser.setId(1L);
-        testUser.setEmail("hello221@gmail.com");
-        testUser.setFirstName("Petr");
-        testUser.setLastName("Prunov");
-        testUser.setPassword("Password123@");
-        userRepository.save(testUser);
-
-        Profile testProfile = new Profile();
-        testProfile.setId(2L);
-        testProfile.setUser(testUser);
-        profileRepository.save(testProfile);
-
-        Long userId = testUser.getId();
-        Long profileId = testProfile.getId();
-
-        String token = jwtTokenUtils.generateToken(testUser);
+    public void testSuccessfulAccountDeletionRequest() throws Exception {
+        when(userService.sendAccountDeletionRequest(anyLong())).thenReturn(true);
 
         mockMvc.perform(delete("/auth/delete")
-                        .header("Authorization", "Bearer " + token))
-                .andExpect(status().isOk());
-
-        Optional<User> deletedUser = userRepository.findById(userId);
-        Optional<Profile> deletedProfile = profileRepository.findById(profileId);
-
-        assertThat(deletedUser).isEmpty();
-
-        assertThat(deletedProfile).isEmpty();
+                        .header("Authorization", "Bearer validToken"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Account deletion request sent to email."));
     }
 
     @Test
-    @Transactional
-    public void testDeleteUserNotFound() throws Exception {
-        User nonExistentUser = new User();
-        nonExistentUser.setId(9999L);
-        nonExistentUser.setEmail("nonexistentuser@example.com");
-        nonExistentUser.setPassword("NonExistentUserPassword123");
-        nonExistentUser.setFirstName("NonExistent");
-        nonExistentUser.setLastName("User");
-
-        String nonExistentUserToken = jwtTokenUtils.generateToken(nonExistentUser);
+    public void testUserNotFound() throws Exception {
+        when(jwtTokenUtils.getCurrentUserId(anyString())).thenReturn(null);
 
         mockMvc.perform(delete("/auth/delete")
-                        .header("Authorization", nonExistentUserToken))
-                .andExpect(status().isNotFound());
+                        .header("Authorization", "Bearer validToken"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("User not found."));
     }
 
     @Test
-    public void testDeleteUserUnauthorized() {
-        String invalidToken = "Bearer InvalidToken";
+    public void testFailedToSendAccountDeletionRequest() throws Exception {
+        when(userService.sendAccountDeletionRequest(anyLong())).thenReturn(false);
 
+        mockMvc.perform(delete("/auth/delete")
+                        .header("Authorization", "Bearer validToken"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Failed to send account deletion request."));
+    }
+
+    @Test
+    public void testExceptionDuringOperation() {
         try {
-            mockMvc.perform(delete("/auth/delete")
-                            .header("Authorization", invalidToken))
-                    .andExpect(status().isUnauthorized());
+            ResultActions result = mockMvc.perform(delete("/auth/delete")
+                            .header("Authorization", "Bearer validToken"))
+                    .andExpect(status().isNotFound());
 
-            assertTrue(true);
-        } catch (jakarta.servlet.ServletException e) {
-            if (e.getCause() instanceof org.springframework.dao.InvalidDataAccessApiUsageException &&
-                    e.getCause().getMessage().contains("The given id must not be null")) {
-                assertTrue(true);
-            } else {
-                fail("Unexpected error: " + e.getMessage());
-            }
+            int status = result.andReturn().getResponse().getStatus();
+            assertEquals(404, status, "Статус должен быть 500");
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
+    }
+
+    @Test
+    public void testInvalidAuthorizationToken() throws Exception {
+        mockMvc.perform(delete("/auth/delete")
+                        .header("Authorization", "Bearer invalidToken"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Failed to send account deletion request."));
     }
 }
