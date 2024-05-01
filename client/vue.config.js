e6a6d179eaf0e89bf1038356a5c79b43d7fdd371
VovaDVA <vl82d@mail.ru>
2024-05-01 21:13:05 +0300

    [JT-F-059] implement message copying, editing and deleting in chat

--- client/vue.config.js
+++ client/vue.config.js
@@ -19,6 +19,13 @@ module.exports = defineConfig({
 					'^/chat': '/chat'
 				}
 			},
+			'/message': {
+				target: 'http://127.0.0.1:3000',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/message': '/message'
+				}
+			},
 			'/post': {
 				target: 'http://127.0.0.1:3001',
 				changeOrigin: true,
